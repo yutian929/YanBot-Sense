821634504eeb7f028375646761e7d4e2cdad9ee5
yutian929 <yutian929@outlook.com>
2025-02-04 11:07:13 +0800

    renew format.

--- scripts/install_deps_basic.sh
+++ scripts/install_deps_basic.sh
@@ -24,17 +24,3 @@ sudo apt install ros-${ROS_DISTRO}-ddynamic-reconfigure
 check_success
 sudo apt install v4l-utils
 check_success
-
-# ## docker
-# sudo apt-get update
-# sudo apt-get install ca-certificates curl
-# sudo install -m 0755 -d /etc/apt/keyrings
-# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-# sudo chmod a+r /etc/apt/keyrings/docker.asc
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt-get update
-# sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# sudo docker run hello-world
